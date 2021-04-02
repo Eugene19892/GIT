1f6cb32304c8ee5309eb493ef64503e94e8781b2
Eugene19892 <81512272+Eugene19892@users.noreply.github.com>
2021-04-02 11:41:30 +0300

    Update HW2.java

--- src/lesson2/HW2.java
+++ src/lesson2/HW2.java
@@ -66,22 +66,3 @@ public class HW2 {
 //   }
 //
 //
-//public static int Max(int[] inputArray){
-//        int maxValue = inputArray[0];
-//        for(int i=1;i < inputArray.length;i++){ if(inputArray[i] > maxValue){
-//        maxValue = inputArray[i];
-//        }
-//        }
-//        return maxValue;
-//        }
-//
-//public static int Min(int[] inputArray){
-//        int minValue = inputArray[0];
-//        for(int i=1;i<inputArray.length;i++){
-//        if(inputArray[i] < minValue){
-//        minValue = inputArray[i];
-//        }
-//        }
-//        return minValue;
-//        }
-//
